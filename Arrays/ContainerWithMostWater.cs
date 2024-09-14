54f2ade676e9b46c6642b94e80fe444718ecd38c
Nawfel Hamdi <nawfel.hamdi@tn.ey.com>
2024-09-14 12:40:35 +0100

    finish up container with most water section

--- Arrays/ContainerWithMostWater.cs
+++ Arrays/ContainerWithMostWater.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,12 +16,23 @@ namespace FAANGInterviewQuestions.Arrays
     {
         public static void Execute()
         {
-            var height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
-            var result = MaxArea(height);
-            Console.WriteLine(result);
+            //var height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            //var height = new int[] { 2, 3, 4, 5, 18, 17, 6 };
+            //var height = new int[] { 1, 8, 6, 2, 5, 4, 8, 25, 7 };
+            //var height = new int[] { 1,2 };
+            //var height = new int[] { 1, 8, 100, 2, 100, 4, 8, 3, 7 };
+            var height = new int[] { 1, 2, 4, 3 };
+            //var height = new int[] { 1, 2, 1 };
+            var result1 = MaxArea1(height);
+            var result2 = MaxArea2(height);
+            var result3 = MaxArea3(height);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
         }
 
-        public static int MaxArea(int[] height)
+        // Mine Solution 1
+        public static int MaxArea1(int[] height)
         {
             var areas = new List<int>();
             for (int i = 0; i < height.Length; i++)
@@ -35,6 +48,49 @@ namespace FAANGInterviewQuestions.Arrays
             }
 
             return areas.Max();
+        }
+
+        // Coorection 1
+        // a = Min(b,a) * (bi - ai)
+        // TIME : O(n*n)
+        // SPACE : O(1)
+        public static int MaxArea2(int[] height)
+        {
+            var maxArea = 0;
+            for(int i = 0; i < height.Length;i++)
+            {
+                for(int j = i + 1; j < height.Length; j++)
+                {
+                    var h = Math.Min(height[i], height[j]);
+                    var w = j - i;
+                    var area = h * w;
+                    maxArea = Math.Max(maxArea, area);
+                }
+            }
+            return maxArea;
+        }
+
+        public static int MaxArea3(int[] height)
+        {
+            var p1 = 0;
+            var p2 = height.Length - 1;
+            var maxArea = 0;
+            while (p1 < p2)
+            {
+                var w = p2 - p1;
+                var h = Math.Min(height[p1], height[p2]);
+                var area = h * w;   
+                maxArea = Math.Max(maxArea, area);
+                if(height[p1] <= height[p2])
+                {
+                    p1++;
+                }else
+                {
+                    p2--;
+                }
+            }
+
+            return maxArea;
         }
     }
 }
