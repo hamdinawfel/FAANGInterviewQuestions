9eb3233993a32c22abac4422cd419518abc0d9c1
Nawfel Hamdi <nawfel.hamdi@tn.ey.com>
2024-09-27 16:56:34 +0100

    implement IsPalindrome solution with reverse strategie

--- Strings/Palindrome.cs
+++ Strings/Palindrome.cs
@@ -13,47 +13,71 @@ namespace FAANGInterviewQuestions.Strings
             //var s1 = "0b";
             //var s1 = ",; W;:GlG:;l ;,";
             var s1 = "Suuv,5?tt?5,vYYS";
+            var s2 = "race a car";
             //var s1 = "abb";
             //var s1 = ".,";
-            Console.WriteLine(IsPalindrome(s1));
+            Console.WriteLine(IsPalindrome3(s));
             //Output: true
             //Explanation: "amanaplanacanalpanama" is a palindrome.
 
         }
 
+        //MINE 
         public static bool IsPalindrome(string s)
         {
-            var sb = new StringBuilder();
-            foreach (char c in s)
-            {
-               if(!char.IsWhiteSpace(c) && (char.IsLetter(c) || char.IsDigit(c)))
-               {
-                    sb.Append(char.ToLower(c));
-               }
-            }
+            var sb = CleanUp(s);
 
             if (sb.Length >= 2)
             {
-
-                if(sb.Length == 0 || sb.Length == 1)
+                var left = sb.ToString().Substring(0, sb.Length / 2);
+                var right = sb.ToString().Substring((sb.Length / 2) + sb.Length % 2);
+                for (var i = 0; i < left.Length; i++)
                 {
-                    return true;
-                }
-                else
-                {
-                    var left = sb.ToString().Substring(0, sb.Length / 2);
-                    var right = sb.ToString().Substring((sb.Length / 2) + sb.Length % 2);
-                    for ( var i = 0; i < left.Length; i++)
+                    if (left[i] != right[left.Length - 1 - i])
                     {
-                        if (left[i] != right[left.Length -1 - i]) return false;
+                        return false;
                     }
-                    return true;
                 }
+                return true;
             }
             else
             {
                 return true;
             }
+        }
+
+        //AFTER HINT: P1 = 0 ; P2 = s.Length -1 and GO to the middel
+        public static bool IsPalindrome1(string s)
+        {
+            return false;
+        }
+
+        //AFTER HINT: P1 = Middle ; P2 = Middle and GO to the extrimity
+        public static bool IsPalindrome2(string s)
+        {
+            return false;
+        }
+
+        //AFTER HINT: clean up - reverse - and compare
+        public static bool IsPalindrome3(string s)
+        {
+            var sb = CleanUp(s);
+            var reversed = string.Join("",sb.ToString().Reverse());
+            return reversed.Equals(sb.ToString());
+
+        }
+
+        private static StringBuilder CleanUp(string s)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (!char.IsWhiteSpace(c) && (char.IsLetter(c) || char.IsDigit(c)))
+                {
+                    sb.Append(char.ToLower(c));
+                }
+            }
+            return sb;
         }
     }
 }
